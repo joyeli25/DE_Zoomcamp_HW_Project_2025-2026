2d770d5c89fca0803ba64db5bcdc90844c4b4791
Joye <68166902+joyeli25@users.noreply.github.com>
2026-04-12 06:30:21 +0000

    commit 1234567890abcdef1234567890abcdef12345678

--- Project/03_dbt_Data_Transformation/chi_crime_project/models/mart/dim_iucr.sql
+++ Project/03_dbt_Data_Transformation/chi_crime_project/models/mart/dim_iucr.sql
@@ -1,7 +1,7 @@
 select
 IUCR as iucr,
-"PRIMARY DESCRIPTION" as primary_description,
-"SECONDARY DESCRIPTION" as secondary_description,
-"INDEX CODE" as index_code,
+PRIMARY_DESCRIPTION as primary_description,
+SECONDARY_DESCRIPTION as secondary_description,
+INDEX_CODE as index_code,
 ACTIVE as active
 from {{ ref('iucr_code_lookup') }}
