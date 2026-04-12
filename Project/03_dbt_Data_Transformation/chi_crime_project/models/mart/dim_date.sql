fed70149d083c29adfdaf8f36ecadc9f1f5543bc
Joye <68166902+joyeli25@users.noreply.github.com>
2026-04-12 06:19:22 +0000

    commit 1234567890abcdef1234567890abcdef12345678

--- Project/03_dbt_Data_Transformation/chi_crime_project/models/mart/dim_date.sql
+++ Project/03_dbt_Data_Transformation/chi_crime_project/models/mart/dim_date.sql
@@ -6,9 +6,9 @@
 
 select distinct
     Date as date,  --"01/07/2026 05:17:00 PM"
-    cast(PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', "date") as date) as formatted_date,
-    extract(year from PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', "date")) as year,
-    extract(month from PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', "date")) as month,
-    extract(day from PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', "date")) as day,
-    format_date('%A', cast(PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', "date") as date)) as weekday
+    cast(PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', crime_date) as date) as formatted_date,
+    extract(year from PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', crime_date)) as year,
+    extract(month from PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', crime_date)) as month,
+    extract(day from PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', crime_date)) as day,
+    format_date('%A', cast(PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', crime_date) as date)) as weekday
 from {{ ref('stg_chi_crime_all') }}
